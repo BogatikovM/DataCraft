2eb2eccd673706aa57206d3c3af616bf4aa02312
tshatanov <tshatanov@truvisibility.com>
2024-10-25 20:36:56 +0300

    add env upload

--- app/main.py
+++ app/main.py
@@ -1,13 +1,20 @@
+import os
 from fastapi import FastAPI
 from motor.motor_asyncio import AsyncIOMotorClient
+from dotenv import load_dotenv
 
+load_dotenv()
 app = FastAPI()
 
 # MongoDB connection
+# Получение значений из переменных окружения
+MONGODB_URL = os.getenv("MONGODB_URL")
+MONGODB_DB_NAME = os.getenv("MONGODB_DB_NAME")
 @app.on_event("startup")
 async def startup_db_client():
-    app.mongodb_client = AsyncIOMotorClient("mongodb://localhost:27017")
-    app.mongodb = app.mongodb_client["mydatabase"]
+    print(MONGODB_DB_NAME)
+    app.mongodb_client = AsyncIOMotorClient(MONGODB_URL)
+    app.mongodb = app.mongodb_client[MONGODB_DB_NAME]
 
 @app.on_event("shutdown")
 async def shutdown_db_client():
